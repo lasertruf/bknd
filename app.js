18f040be1ce3070b5a07f0659c8ae2e8f442316c
lasertruf <jayantwaghmare9@gmail.com>
2021-07-21 16:18:46 +0530

    okok

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
-const http = require('http');
-const app = require('./backend/app');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const port = process.env.PORT || 3000;
+var indexRouter = require('./route');
 
-app.set('port',port);
 
-const server = http.createServer(app);
-console.log("Running")
+var app = express();
 
-server.listen(port);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const app = require('./backend/app');
+
+const port = process.env.PORT || 3000;
+
+app.set('port',port);
+
+const server = http.createServer(app);
+console.log("Running")
+
+server.listen(port);
